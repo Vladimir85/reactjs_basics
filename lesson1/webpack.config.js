8893b67cf50a3702d2fa9cdc424b75790d4cc12a
Vladimir Banishevskii <a-vbanishevskii@expedia.com>
2017-11-15 09:18:55 -0600

    Removed hash prepending on compile

--- lesson1/webpack.config.js
+++ lesson1/webpack.config.js
@@ -2,20 +2,19 @@ const webpack             = require('webpack')
 const path                = require('path')
 const HtmlWebpack         = require('html-webpack-plugin')
 const ExtractTextPlugin   = require("extract-text-webpack-plugin")
-const hashSize            = 8
 const assetsHashGenerator = new webpack.HashedModuleIdsPlugin()
 
 const htmlInject = new HtmlWebpack({ template: './app/index.html' })
 
 // Compile less files into separate CSS file
 const extractLess = new ExtractTextPlugin({
-  filename: `[contenthash:${hashSize}].app.css`
+  filename: 'app.css'
 })
 
 // Combine app dependencies into separate file
 const vendorChunk = new webpack.optimize.CommonsChunkPlugin({
   name      : 'vendor',
-  filename  : `[chunkhash:${hashSize}].vendor.js`,
+  filename  : 'vendor.js',
   // this assumes your vendor imports exist in the node_modules directory
   minChunks : ({ context }) => context && context.indexOf('node_modules') !== -1
 })
@@ -53,7 +52,7 @@ module.exports = (env) => ({
   },
   output : {
     path     : path.resolve(__dirname, 'build'),
-    filename : `[chunkhash:${hashSize}].app.js`
+    filename : 'app.js'
   },
   module: {
     rules: [
