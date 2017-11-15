1122a5e9d69c7974cb460b02c1e39d1d8d181fe3
Vladimir Banishevskii <a-vbanishevskii@expedia.com>
2017-11-15 10:21:27 -0600

    Created empty app

---
+++ empty_project/app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import styles from './less/main.less'
+
+render(
+  <div>
+    <h1>Renderd with ReactJS</h1>
+  </div>,
+  document.getElementById('app-container')
+)
